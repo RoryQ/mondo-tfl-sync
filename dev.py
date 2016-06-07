61d000df33a0bb1764c0453c49deca1029d87bca
Rory Quinn <rory@roryq.com>
2016-06-07 23:10:08 +0100

    save state_token in session and check in callback

--- dev.py
+++ dev.py
@@ -2,13 +2,16 @@ from os import environ
 from mondo.authorization import (generate_state_token,
                                  generate_mondo_auth_url,
                                  exchange_authorization_code_for_access_token)
-from flask import Flask, request
+from flask import Flask, request, session, redirect
 
 app = Flask(__name__)
 
 client_id = environ['MONDO_CLIENT_ID']
 client_secret = environ['MONDO_CLIENT_SECRET']
 redirect_uri = environ['MONDO_REDIRECT_URI']
+secret_key = environ['SECRET_KEY']
+
+app.secret_key = secret_key
 
 @app.route('/')
 def hello_world():
@@ -16,16 +19,23 @@ def hello_world():
 
 @app.route('/login')
 def login():
+    state_token = generate_state_token()
+    session['state_token'] = state_token
     auth_url = generate_mondo_auth_url(
         client_id=client_id,
-        redirect_uri=redirect_uri
+        redirect_uri=redirect_uri,
+        state_token=state_token
         )
-    return auth_url
+    return redirect(auth_url)
 
 @app.route('/oauth', methods=['GET'])
 def oauth():
     auth_code = request.args.get('code', '')
     state = request.args.get('state', '')
+
+    if state != session['state_token']:
+        print("state: {} different to session state {}".format(state, session['state_token']))
+        return "Something bad happened!"
 
     print("auth_code: {}".format(auth_code))
 
